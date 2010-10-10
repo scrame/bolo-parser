b58d3bd32ddaf3a88a0ab9fcd6e23a8240a53430
screaming scramechan <scrame@scrame.com>
2010-10-10 01:21:36 -0700

    factoring file parsing to a single function.

--- map_read.py
+++ map_read.py
@@ -1,12 +1,5 @@
 #!/usr/bin/python
 
-##### TODO: Handle with commandline
-map_file= "maps/Fitzhu.map"
-
-print("Opening: " + map_file);
-
-fd = open(map_file, "rb");
-
 def read_byte():
     return ord(fd.read(1))
 
@@ -204,115 +197,117 @@ class Run:
         + ("\nnibblets:\t" + str(self.get_nibblets()))
         + ("\ndata:\t" + str(self.data)))
 
+def parse_file(fd):
+    #Start of reading!
+    #assumes fd is a file descriptor.
 
-#Start of reading!
-#assumes fd is a file descriptor.
+    header = str(fd.read(8))
 
-header = str(fd.read(8))
+    #File identifier
+    if("BMAPBOLO" != header):
+         print "Error! Invalid or corrupted map file"
+         exit(0)
 
-#File identifier
-if("BMAPBOLO" != header):
-    print "Error! Invalid or corrupted map file"
-    exit(0)
-
-print header + " looks good."
-
-
-#for now, print statements, and variables describe the process.
-#The rest of the script procedurally creates the map.
-
-map_version = read_byte()
-print("Map version: " + str(map_version))
-
-num_pillboxes = read_byte()
-print("No. Pillboxes: " + str(num_pillboxes))
-
-num_refuelling_stations = read_byte()
-print("No. Refuelling Stations: " + str(num_refuelling_stations))
-
-num_tank_starting_squares = read_byte()
-print("No. Tank starting squares: " + str(num_tank_starting_squares))
+    print header + " looks good."
 
 
+    #for now, print statements, and variables describe the process.
+    #The rest of the script procedurally creates the map.
 
-print("Parsing Pillboxes")
-pillboxes = []
-for i in range(num_pillboxes):
-    print("Pillbox no: " + str(i))
-    pillbox = Pillbox()
-    pillbox.x = read_byte()
-    pillbox.y = read_byte()
-    pillbox.owner = read_byte()
-    pillbox.armor = read_byte()
-    pillbox.speed = read_byte()
-    print(pillbox)
-    pillboxes.append(pillbox)
+    map_version = read_byte()
+    print("Map version: " + str(map_version))
 
-print("Parsing Refuelling Stations")
-refuellers = []
-for i in range(num_refuelling_stations):
-    print("Refueller no: " + str(i) )
-    refueller = Refueller()
-    refueller.x = read_byte()
-    refueller.y = read_byte()
-    refueller.owner = read_byte()
-    refueller.armor = read_byte()
-    refueller.shells = read_byte()
-    refueller.mines = read_byte()
-    print(refueller)
-    refuellers.append(refueller)
+    num_pillboxes = read_byte()
+    print("No. Pillboxes: " + str(num_pillboxes))
 
-print("Parsing Starting Squares")
-starting_squares = []
-for i in range(num_tank_starting_squares):
-    print("Starter no: " + str(i) )
-    start = StartingPoint()
-    start.x = read_byte()
-    start.y = read_byte()
-    start.dir = read_byte()
-    print(start)
-    starting_squares.append(start)
+    num_refuelling_stations = read_byte()
+    print("No. Refuelling Stations: " + str(num_refuelling_stations))
+
+    num_tank_starting_squares = read_byte()
+    print("No. Tank starting squares: " + str(num_tank_starting_squares))
 
 
-map = [[None for i in range(256)] for j in range(256)]
 
-runs = []
-print("Parsing runs.")
-keep_running = True
+    print("Parsing Pillboxes")
+    pillboxes = []
+    for i in range(num_pillboxes):
+        print("Pillbox no: " + str(i))
+        pillbox = Pillbox()
+        pillbox.x = read_byte()
+        pillbox.y = read_byte()
+        pillbox.owner = read_byte()
+        pillbox.armor = read_byte()
+        pillbox.speed = read_byte()
+        print(pillbox)
+        pillboxes.append(pillbox)
 
-count = 0
-while(keep_running):
-    print "Run no:" + str(count)
-    run = Run()
-    run.datalen = read_byte()
-    run.y = read_byte()
-    run.startx = read_byte()
-    run.endx = read_byte()
-    raw_data = fd.read(run.datalen - 4) # MAGIC NUMBER: datalen includes the length of the header. 
+    print("Parsing Refuelling Stations")
+    refuellers = []
+    for i in range(num_refuelling_stations):
+        print("Refueller no: " + str(i) )
+        refueller = Refueller()
+        refueller.x = read_byte()
+        refueller.y = read_byte()
+        refueller.owner = read_byte()
+        refueller.armor = read_byte()
+        refueller.shells = read_byte()
+        refueller.mines = read_byte()
+        print(refueller)
+        refuellers.append(refueller)
+
+    print("Parsing Starting Squares")
+    starting_squares = []
+    for i in range(num_tank_starting_squares):
+        print("Starter no: " + str(i) )
+        start = StartingPoint()
+        start.x = read_byte()
+        start.y = read_byte()
+        start.dir = read_byte()
+        print(start)
+        starting_squares.append(start)
+
+    runs = []
+    print("Parsing runs.")
+    keep_running = True
+
+    count = 0
+    while(keep_running):
+        print "Run no:" + str(count)
+        run = Run()
+        run.datalen = read_byte()
+        run.y = read_byte()
+        run.startx = read_byte()
+        run.endx = read_byte()
+        raw_data = fd.read(run.datalen - 4) # MAGIC NUMBER: datalen includes the length of the header. 
                                         # 4 means there is no data!!!
 
-    binary_data = []
-    #parse byte-by-byte into integral data from binary.
-    for i in raw_data:
-        binary_data.append(ord(i))
+        binary_data = []
+        #parse byte-by-byte into integral data from binary.
+        for i in raw_data:
+            binary_data.append(ord(i))
 
-    run.parse_map_data(binary_data)
+        run.parse_map_data(binary_data)
 
-    if(run.isEOF()):
-        print("EOF")
-        keep_running = False
-        break
+        if(run.isEOF()):
+            print("Encountered last run. Exiting.")
+            keep_running = False
+            break
 
-    run.calculate_run()
-
-    runs.append(run)
-
-
-    count += 1
+        run.calculate_run()
+        runs.append(run)
+        count += 1
 
 
 
-print("Encountered last run. Exiting.")
+##### TODO: Handle with commandline
+map_file= "maps/Fitzhu.map"
+
+print("Opening: " + map_file);
+
+fd = open(map_file, "rb");
+
+parse_file(fd)
+
 print("Closing map file...")
 fd.close()
 

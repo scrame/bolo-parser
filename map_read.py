f7ae81be943ee23c82aee2b90fd791e2763f4dcb
screaming scramechan <scrame@scrame.com>
2010-10-10 03:07:05 -0700

    putting init at the top of mapfile

--- map_read.py
+++ map_read.py
@@ -6,6 +6,17 @@ class MapFile:
 
     def __init__(self,map_file):
         fd = open(map_file, "rb");
+
+        self.read_header(fd)
+        self.read_pillboxes(fd)
+        self.read_refuellers(fd)
+        self.read_starting_squares(fd)
+        self.read_runs(fd)
+
+        fd.close()
+        fd = None
+
+    def read_header(self,fd):
         self.header = str(fd.read(8))
 
         if("BMAPBOLO" != self.header):
@@ -16,7 +27,9 @@ class MapFile:
         self.num_pillboxes = ord(fd.read(1))
         self.num_refuelling_stations = ord(fd.read(1))
         self.num_tank_starting_squares = ord(fd.read(1))
+        
 
+    def read_pillboxes(self,fd):
         self.pillboxes = []
         for i in range(self.num_pillboxes):
             pillbox = Pillbox()
@@ -26,7 +39,8 @@ class MapFile:
             pillbox.armor = ord(fd.read(1))
             pillbox.speed = ord(fd.read(1))
             self.pillboxes.append(pillbox)
-
+            
+    def read_refuellers(self,fd):
         self.refuellers = []
         for i in range(self.num_refuelling_stations):
             refueller = Refueller()
@@ -38,6 +52,7 @@ class MapFile:
             refueller.mines = ord(fd.read(1))
             self.refuellers.append(refueller)
 
+    def read_starting_squares(self,fd):
         self.starting_squares = []
         for i in range(self.num_tank_starting_squares):
             start = StartingPoint()
@@ -46,10 +61,9 @@ class MapFile:
             start.dir = ord(fd.read(1))
             self.starting_squares.append(start)
 
+    def read_runs(self,fd):
         self.runs = []
         keep_running = True
-
-        count = 0
         while(keep_running):
             run = Run()
             run.datalen = ord(fd.read(1))
@@ -65,13 +79,11 @@ class MapFile:
             run.parse_map_data(binary_data)
 
             if(run.isEOF()):
-                #print("Encountered last run. Exiting.")
                 keep_running = False
                 break
 
             run.calculate_run()
             self.runs.append(run)
-            count += 1
 
-        fd.close()
+        
 
